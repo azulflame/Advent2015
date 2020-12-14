b0a86050f1d936ce09806e450df099f65aaf02b8
Todd Bensmiller <toddbjta@gmail.com>
2020-12-14 09:05:10 -0600

    Day 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,5 +1,7 @@
 package me.toddbensmiller.advent
 
+import java.util.Collections.min
+
 fun day2(): Pair<Number,Number>
 {
 	return Pair(day2part1(ListHolder.day2),day2part2(ListHolder.day2))
@@ -7,9 +9,30 @@ fun day2(): Pair<Number,Number>
 
 fun day2part1(input: List<String>): Number
 {
-	return 0
+	var totalPaper = 0
+	val parsed = input.map { a -> a.split('x').map { b -> b.toInt()}}
+	for(line in parsed)
+	{
+		val sideA = line[0]*line[1]
+		val sideB = line[0]*line[2]
+		val sideC = line[1]*line[2]
+		val smallSide = min(setOf(sideA, sideB, sideC))
+		totalPaper += 2*sideA + 2*sideB + 2*sideC + smallSide
+	}
+	return totalPaper
 }
 fun day2part2(input: List<String>): Number
 {
-	return 0
+	var totalRibbon = 0
+	val parsed = input.map { a -> a.split('x').map { b -> b.toInt()}}
+	for(line in parsed)
+	{
+		val lenA = line[0]
+		val lenB = line[1]
+		val lenC = line[2]
+		val sides = listOf(lenA,lenB,lenC).sorted()
+		val ribbonLength = 2*sides[0] + 2*sides[1] + lenA*lenB*lenC
+		totalRibbon += ribbonLength
+	}
+	return totalRibbon
 }
